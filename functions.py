9de222ebe8aa60bad28645797530a195e2ef7f32
quiqueferre12 <quiqueferre12@gmail.com>
2023-03-22 11:31:58 +0100

    commit_5

--- functions.py
+++ functions.py
@@ -12,13 +12,29 @@ def read_data(fichero):
         for row in reader:
             if len(row)==13:
                 var=nombre+str(cont)
-                dic={ var :{ "type": row[0],"fixed acidity": row[1], "volatile acidity": row[2], "citric acidity": row[3], "residual sugar": row[4],
+                dic.update(var ={ "type": row[0],"fixed acidity": row[1], "volatile acidity": row[2], "citric acidity": row[3], "residual sugar": row[4],
                        "chlorides": row[5], "free sulfur dioxide": row[6], "total sulfur dioxide": row[7],
-                       "density":row[8], "PH":row[9], "sulphates": row[10], "alcohol": row[10], "quality":row[11]} } 
+                       "density":row[8], "PH":row[9], "sulphates": row[10], "alcohol": row[10], "quality":row[11]} )  
                 cont=cont+1
-    print(dic)
+    return dic
+
+def split(diccionario):
+
+    dicRed={}
+    dicWhite={}
+    for i in diccionario:
+        if diccionario[i]["type"] == "red":
+            diccionario[i].pop("type")
+            dicRed.update(diccionario[i])
+        else:
+            diccionario[i].pop("type")
+            dicWhite.update(diccionario[i])
+    return dicRed, dicWhite
 
 
-read_data(directorio)
+
+dicionario =read_data(directorio)
+dicRojo,dicBlanc =split(dicionario)
+
 
 
