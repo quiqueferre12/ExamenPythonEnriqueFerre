3f169161bb5133cdc906027255fe0461d0892129
quiqueferre12 <quiqueferre12@gmail.com>
2023-03-22 11:12:19 +0100

    commit_4

--- functions.py
+++ functions.py
@@ -4,10 +4,20 @@ import csv
 directorio=("D://Universidad3Curso//examen_python_EnriqueFerreCarbonell//ExamenPythonEnriqueFerre//winequality.csv")
 
 def read_data(fichero):
+    dic={}
+    cont=1
+    nombre="dato"
     with open(fichero,'r') as file:
         reader = csv.reader(file)
         for row in reader:
-            print(row)
+            if len(row)==13:
+                var=nombre+str(cont)
+                dic={ var :{ "type": row[0],"fixed acidity": row[1], "volatile acidity": row[2], "citric acidity": row[3], "residual sugar": row[4],
+                       "chlorides": row[5], "free sulfur dioxide": row[6], "total sulfur dioxide": row[7],
+                       "density":row[8], "PH":row[9], "sulphates": row[10], "alcohol": row[10], "quality":row[11]} } 
+                cont=cont+1
+    print(dic)
+
 
 read_data(directorio)
 
